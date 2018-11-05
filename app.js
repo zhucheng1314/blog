2f9802ad5a922586611451c0bc51ae5a5ed465c1
zhucheng <17607122630@163.com>
2018-11-05 21:31:19 +0800

    完成登录 注册页面

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const express=require('express')
 const app=express()
+const bodyparser=require('body-parser')
+
+
+//导入首页路由模块
+const indexrouter=require('./router/index.js')
+//导入用户功能路由模块
+const userrouter=require('./router/user.js')
 
 //设置采用的模板引擎名称
 app.set('view engine','ejs')
 app.set('views','./views')
 
+
+//注册解析表单的数据的中间件
+app.use(bodyparser.urlencoded({extended:false}))
+
+
+//给node_modules挂载虚拟路径
 app.use('/node_modules',express.static('./node_modules'))
 
-app.get('/',(req,res)=>{
-    res.render('index.ejs',{})
-})
+//挂载首页路由模块
+app.use(indexrouter)
+
+
+//挂载用户功能模块
+app.use(userrouter)
+
 
 
 app.listen(80,()=>{
